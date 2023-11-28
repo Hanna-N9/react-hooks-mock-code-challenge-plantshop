2d7e175f18bacabe876d8282c1e596fdc770627e
Hanna Negash <hnegash.n@gmail.com>
2023-11-27 17:56:38 -0800

    Use GET, POST requests and search method for components

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(setPlants);
+  }, []);
+
+  const displayPlantName = plants.filter(plant =>
+    plant.name.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={displayPlantName} />
     </main>
   );
 }
